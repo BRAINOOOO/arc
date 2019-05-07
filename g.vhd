a4964e835179b95dd1fa4ffff3cfc4f69bc57938
BRAINOOOO <38901623+BRAINOOOO@users.noreply.github.com>
2019-05-07 11:29:14 +0200

    Update g.vhd

--- g.vhd
+++ g.vhd
@@ -1,55 +1,50 @@
 ----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date:    09:58:57 04/24/2019 
--- Design Name: 
--- Module Name:    g - Behavioral 
--- Project Name: 
--- Target Devices: 
--- Tool versions: 
--- Description: 
---
--- Dependencies: 
---
--- Revision: 
--- Revision 0.01 - File Created
--- Additional Comments: 
---
+-- MariamSalah project MIPS processor.
+-- shiftleftby2
 ----------------------------------------------------------------------------------
------------------------- LIBRERIE ---------------------------------------
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.std_logic_unsigned.all;
-
 ----------------------- ENTITY --------------------------------------------
 entity shiftleftby2 is
-	-- use a generic to generalize the number of inputs/output defined as standard like 1
+	
+	-- as i always do some generics :)
+	
 	generic (n_in: integer := 32;
-				n_out: integer := 32
-				);
+		 n_out: integer := 32
+		 );
 
-	--port description
-	port (
+	--the port des.
+	port 
+	(
 		input: in std_logic_vector(n_in-1 downto 0);
 		z: out std_logic_vector(n_out-1 downto 0)
 	);
+
 end shiftleftby2;
 
 architecture arc_shiftleftby2 of shiftleftby2 is
 
 
 begin
+	
 	process(input)
-	constant ZERO: std_logic_vector(n_out-1 downto 0) := (others => '0'); -- a way to set all constant to zero
+	constant ZERO: std_logic_vector(n_out-1 downto 0) := (others => '0');
+        -- new way i learned to set all constant to zero
+
 	begin 
-		if (n_in = n_out) then
+		if (n_in = n_out) 
+			then
 			z <= input(n_in-3 downto 0) & "00";
-		elsif(n_in = n_out - 2) then
+		
+		elsif(n_in = n_out - 2) 
+			then
 			z <= "00" & (input(n_in-3 downto 0) & "00");
+		
 		else
 			z <= ZERO;
 		end if;
+			
 	end process;
 
 end arc_shiftleftby2;
